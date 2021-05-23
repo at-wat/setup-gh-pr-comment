a4dc837ef98190a273c4963ce996cb8f8ca4ef50
Atsushi Watanabe <atsushi.w@ieee.org>
2021-05-24 00:09:38 +0900

    Revert debug

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,6 @@ type Release = Endpoints['GET /repos/{owner}/{repo}/releases/latest']['response'
 
 const [owner, repo] = ['at-wat', 'gh-pr-comment']
 
-// test
-
 let osPlat: string = os.platform()
 if (osPlat === 'win32') {
   osPlat = 'windows'
