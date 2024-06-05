3047c4c9ebc72cafa505dbb5cb0537737ef0839d
Atsushi Watanabe <atsushi.w@ieee.org>
2024-06-05 15:52:20 +0900

    Fix eslint typescript pluging

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 2015,
@@ -14,5 +17,4 @@ export default [
       "node_modules/**",
     ],
   },
-  js.configs.recommended,
 ]
